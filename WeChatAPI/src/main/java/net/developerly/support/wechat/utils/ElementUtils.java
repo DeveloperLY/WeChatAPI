7079dfbd61c0bb10daa5be775331660a2d94e757
LY <coderyliu@gmail.com>
2017-06-03 21:06:19 +0800

    添加Element和File工具类

---
+++ WeChatAPI/src/main/java/net/developerly/support/wechat/utils/ElementUtils.java
@@ -0,0 +1,19 @@
+package net.developerly.support.wechat.utils;
+
+/**
+ * Element工具类
+ *
+ * @author LY
+ * @create 2017/06/03
+ **/
+public class ElementUtils {
+    public static final String XML_ELEMENT_ROOT             = "xml";
+    public static final String XML_ELEMENT_MSGTYPE          = "MsgType";
+    public static final String XML_ELEMENT_TOUSERNAME       = "ToUserName";
+    public static final String XML_ELEMENT_FROMUSERNAME     = "FromUserName";
+    public static final String XML_ELEMENT_CREATETIME       = "CreateTime";
+    public static final String XML_ELEMENT_CONTENT          = "Content";
+    public static final String XML_ELEMENT_MSGID            = "MsgId";
+    public static final String XML_ELEMENT_EVENT            = "Event";
+    public static final String XML_ELEMENT_TICKET           = "Ticket";
+}
